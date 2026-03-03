8630a443d95a36eb9375d7c00d6d848029ea0135
alcatdra <alexander.menko.reggie@gmail.com>
2026-03-04 00:24:11 +0300

    fixed card hover on mobile

--- src/app/components/Card/Card.js
+++ src/app/components/Card/Card.js
@@ -1,11 +1,19 @@
-﻿import React from 'react';
+﻿'use client';
+import React, { useState } from 'react';
 import styles from './Card.module.scss';
 
 export default function Card({ word }) {
-  //return <div className={styles.card}>Card</div>;
+  const [flipped, setFlipped] = useState(false);
+
   return (
-    <div className={styles.flipCard}>
-      <div className={styles.flipCardInner}>
+    <div
+      className={`${styles.flipCard} `}
+      onClick={() => setFlipped((f) => !f)}
+      role="button"
+    >
+      <div
+        className={`${styles.flipCardInner} ${flipped ? styles.flipped : ''}`}
+      >
         <div className={styles.flipCardFront}>
           <p>{word.word}</p>
         </div>
